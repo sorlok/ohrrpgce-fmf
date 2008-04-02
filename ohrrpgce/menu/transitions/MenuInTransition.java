ee7cd18d93ad0b75aa1aebb59b4238a9544c6636
seth.hetu <seth.hetu@81c47693-1148-0410-b2fa-1f034b7baa63>
2008-04-02 18:12:19 +0000

    Our menu-in transition works; I'd say it's inspired by KDE, but I hate KDE...

--- ohrrpgce/menu/transitions/MenuInTransition.java
+++ ohrrpgce/menu/transitions/MenuInTransition.java
@@ -3,75 +3,103 @@ package ohrrpgce.menu.transitions;
 import ohrrpgce.adapter.GraphicsAdapter;
 import ohrrpgce.data.RPG;
 
+/**
+ * A simple transition that draws a bunch of circles over the screen, and slowly
+ *   "drags down" the background menu color. 
+ * @author Seth N. Hetu
+ */
 public class MenuInTransition extends Transition {
-	//State tracking
-    private static final int PHASE_ONE = 1;
-   // private static final int PHASE_TWO = 2;
-    private static final int PHASE_DONE = 3;
-    private int phase;
-    
-    //Track our angles...
-    private int currAngle;
+	//Useful constants
+	private static final int boxesPerRow = 5;
     private static final int maxAngle = 360;
-    private static final int angleIncr = 30;
+    private static final int halfTicks = 3; //Determines the speed of everything
+    private static final int angleIncr = maxAngle/(halfTicks*2);
+    private static final int angleHalfIncr = (maxAngle/2)/halfTicks;
+	
+    //Track our progress
+    private int currTick;
     
     //Used internally
     private RPG currRPG;
+    private int menuColor;
     private int width;
     private int height;
+    private int boxSize;
+    private int numBoxRows;
+    private int lastTick;
+    private int tideIncrement;
+    
     
     public MenuInTransition(RPG currRPG, int canvasWidth, int canvasHeight) {
+    	//Save for later
     	this.currRPG = currRPG;
     	this.width = canvasWidth;
     	this.height = canvasHeight;
+    	
+    	//Calculate
+    	boxSize = canvasWidth/boxesPerRow; //Later, scroll left-to-right for horiz. displaysMath.min(canvasHeight, canvasWidth)/5;
+    	numBoxRows = canvasHeight/boxSize;
+    	menuColor = currRPG.getTextBoxColors(0)[0];
+    	tideIncrement = boxSize/halfTicks;
+    	lastTick = (height+boxSize)/tideIncrement;
     	
     	reset();
     }
 
 	public void reset() {
-        currAngle = 0;
-        phase = PHASE_ONE;
+		currTick = 0;
 	}
     
 	
 	public boolean doPaintOver() {
-        int[] clr0 = currRPG.getTextBoxColors(0);
-        int boxBounds = Math.max(width, height);
-        int xStart = (width-boxBounds)/2;
-        int yStart = (height-boxBounds)/2;
+		//We have two things going on here, all painting with the menu's background
+		//  color. First, we are filling up circles from bottom-to-top; second, we 
+		//  are slowly covering the screen with a downward-moving rectangle. These
+		//  things are synchronized.
+		//I'm just going to treat all this like an array access problem, to make
+		//  the math simpler in my head. :)
+        GraphicsAdapter.setColor(menuColor);
         
-        //Draw the arc
-        GraphicsAdapter.setColor(clr0[0]);
-        GraphicsAdapter.fillArc(xStart, yStart, boxBounds, boxBounds, 0, -currAngle);
-                
+        //Draw the circles in THIS row -up to 180 degrees
+        int currRow = currTick/halfTicks;
+        int currAngle = angleHalfIncr*((currTick %halfTicks)+1);
+        int yStart = currRow*boxSize;
+        if (yStart < height) { 
+        	for (int i=0; i<boxesPerRow; i++) {
+        		int xStart = i*boxSize;
+        		GraphicsAdapter.fillArc(xStart, yStart, boxSize, boxSize, -90, -currAngle);
+        	}
+        }
+        
+        //Now, draw the circles in the PREVIOUS row, from 180 to 360 degrees
+        if (--currRow>=0) {
+        	yStart-=boxSize;
+            for (int i=0; i<boxesPerRow; i++) {
+            	int xStart = i*boxSize;
+            	GraphicsAdapter.fillArc(xStart, yStart, boxSize, boxSize, -90, -currAngle-180);
+            }
+        }
+        
+        //Now, draw the rectangle...
+        int currTideline = -boxSize + currTick*tideIncrement;
+        if (currTideline>0)
+        	GraphicsAdapter.fillRect(0, 0, width, currTideline);
+        
         return true;
 	}
 
 
 
 	public boolean step() {
-        switch (phase) {
-        	case PHASE_ONE:
-        		//Are we done?
-        		if (currAngle == maxAngle) {
-        			phase = PHASE_DONE;
-        			break;
-        		}
-            
-        		//Lengthen our arc
-        		if (currAngle<maxAngle) {
-        			currAngle+=angleIncr;
-        			if (currAngle+angleIncr>maxAngle)
-        				currAngle = maxAngle;
-        		}
+		//Are we done?
+		if (currTick == lastTick) {
+			return true;
+		}
+    
+		//Lengthen our arc, tide
+		currTick++;
 
-        		break;
-
-        	default:
-        		throw new RuntimeException("Illegal step: this Transition has finished!");
-        }
- 
-        return (phase==PHASE_DONE);
+		return false;
 	}
 
 }
