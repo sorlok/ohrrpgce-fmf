8cc3a0a9f88e934a104c60a4c000df0a6292cfc4
seth.hetu <seth.hetu@81c47693-1148-0410-b2fa-1f034b7baa63>
2008-04-23 06:56:51 +0000

    Hero selector finally works, which obviates two old classes (yay!)

--- trunk/ohrrpgce/menu/HeroSelectSlice.java
+++ trunk/ohrrpgce/menu/HeroSelectSlice.java
@@ -25,7 +25,6 @@ public class HeroSelectSlice extends MenuSlice {
     private static final int cursorSize = 9;
     private static final int innerSpacing = 3;
     
-    
     //Fields
     private int minElements;
     private int maxElements;
@@ -33,20 +32,22 @@ public class HeroSelectSlice extends MenuSlice {
     private int cursorTLX;
     
     
-    private static ImageSlice[] getPartyAsButtons(Hero[] heroParty) {
+    private static ImageSlice[] getPartyAsButtons(Hero[] heroParty, RPG rpg) {
     	ImageSlice[] ret = new ImageSlice[heroParty.length];
     	MenuFormatArgs mf = new MenuFormatArgs();
     	mf.borderColors = new int[]{};
     	mf.bgColor = 0xFF0000;
-    	mf.fillType = MenuSlice.FILL_SOLID;
+    	mf.fillType = MenuSlice.FILL_NONE;
     	mf.widthHint = MenuFormatArgs.WIDTH_MINIMUM;
     	mf.heightHint = MenuFormatArgs.HEIGHT_MINIMUM;
+    	mf.yHint = cursorSize+2;
         for (int i=0; i<ret.length; i++) {
-        	ret[i] = new ImageSlice(mf, heroParty[i].getWalkabout().spData[4], PictureParser.PT_WALKABOUT_SIZES[0]);
+        	ret[i] = new ImageSlice(mf, heroParty[i].getWalkabout().spData[4], heroParty[i].walkaboutPaletteID, rpg, PictureParser.PT_WALKABOUT_SIZES[0]);
         	
         	if (i==0) {
         		mf.fromAnchor = GraphicsAdapter.TOP|GraphicsAdapter.RIGHT;
             	mf.xHint = innerSpacing;
+            	mf.yHint = 0;
         	} else {
         		ret[i-1].connect(ret[i], MenuSlice.CONNECT_RIGHT, MenuSlice.CFLAG_PAINT);
         	}
@@ -64,23 +65,22 @@ public class HeroSelectSlice extends MenuSlice {
     }
     
 
-    public void setHeroParty(Hero[] heroParty, int partyIDofUser) {
+    public void setHeroParty(Hero[] heroParty, RPG rpg, int partyIDofUser) {
         //Setup
-    	partyPictures = getPartyAsButtons(heroParty);
+    	partyPictures = getPartyAsButtons(heroParty, rpg);
         setIDOfUser(partyIDofUser);
         this.setTopLeftChild(partyPictures[0]);
-        //currActiveChildMI = partyPictures[0];
     }
     
     public void setIDOfUser(int partyIDOfUser) {
         this.cursorTLX = mFormat.borderPadding + partyIDOfUser*(innerSpacing+PictureParser.PT_WALKABOUT_SIZES[0]) + PictureParser.PT_WALKABOUT_SIZES[0]/2 - cursorSize/2;
     }
     
-    public void paintAt(int x, int y) {
-    	//Draw the component
-    	super.paintAt(x, y);
+    protected void drawPixelBuffer(int atX, int atY) {
+    	super.drawPixelBuffer(atX, atY);
     	
     	//Draw the cursor
+//    	System.out.println(getPosX()+cursorTLX + "," + getPosY()+mFormat.borderColors.length+1);
         GraphicsAdapter.drawRGB(cursor, 0, cursorSize, 
                 getPosX()+cursorTLX,
                 getPosY()+mFormat.borderColors.length+1,
@@ -91,6 +91,10 @@ public class HeroSelectSlice extends MenuSlice {
     }
     
     //Important over-rides
+/*    protected void setHeight(int newHeight) {
+    	super.setHeight(newHeight + cursorSize+2);
+    }*/
+    
     /*protected int calcMinWidth() {
         return (innerSpacing+PictureParser.PT_WALKABOUT_SIZES[0])*maxElements - innerSpacing;
     }
