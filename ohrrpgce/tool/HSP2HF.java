30c14ece5a3e8ba170c925f49d65f4748c149e85
seth.hetu <seth.hetu@81c47693-1148-0410-b2fa-1f034b7baa63>
2008-10-03 11:07:24 +0000

    Finally got some decent formatting, and got rid of that nasty signed/unsigned bug.

--- ohrrpgce/tool/HSP2HF.java
+++ ohrrpgce/tool/HSP2HF.java
@@ -3,6 +3,8 @@ package ohrrpgce.tool;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -13,6 +15,8 @@ import java.util.Hashtable;
 import java.util.Vector;
 
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -22,10 +26,17 @@ public class HSP2HF extends JFrame {
 	private JPanel toolbarPnl;
 	private JList scriptNameLst;
 	private JPanel scriptContentsPnl;
+	private int flipflop = 1;
 	
 	//Scripts
 	private DefaultListModel scriptNameModel;
 	private Hashtable idToName;
+	
+	//Sources
+	//private ScriptSourceRenderer scriptSrcModel;
+	//private ArrayList scriptSrcInOrder;
+	private Hashtable atIDToScriptSrc;
+	private ScriptSrc currScriptSrc;
 	
 	
 	public HSP2HF() {
@@ -39,6 +50,16 @@ public class HSP2HF extends JFrame {
 		
 		this.setSize(400, 300);
 		this.setVisible(true);
+	}
+	
+	
+	private void swapIn(ScriptSrc nextSrc) {
+		if (currScriptSrc!=null) {
+			currScriptSrc.flashID(false);
+		}
+		
+		currScriptSrc = nextSrc;
+		currScriptSrc.flashID(true);
 	}
 	
 	
@@ -57,6 +78,7 @@ public class HSP2HF extends JFrame {
 		}
 		
 		//Read header lump
+		int maxStringWidth = -1;
 		if (true) {
 			Lump headerLump = new Lump(hspFile);
 			if (headerLump.inError)
@@ -96,6 +118,7 @@ public class HSP2HF extends JFrame {
 			
 			//Read each name
 			idToName = new Hashtable();
+			atIDToScriptSrc = new Hashtable();
 			Script newScript = new Script();
 			StringBuilder currStr = new StringBuilder();
 			int currID = 0;
@@ -145,6 +168,10 @@ public class HSP2HF extends JFrame {
 					//scriptNameModel.addElement("<script " + i + " does not exist>");
 				} else { 
 					scriptNameModel.addElement(s);
+					
+					int currStringWidth = scriptNameLst.getFontMetrics(scriptNameLst.getFont()).stringWidth(s.toString());
+					if (currStringWidth > maxStringWidth) 
+						maxStringWidth = currStringWidth;
 				}
 				
 			}
@@ -180,7 +207,11 @@ public class HSP2HF extends JFrame {
 		if (scriptNameModel.size()>0) {
 			scriptNameLst.setSelectedIndex(0);
 			scriptNameLst.ensureIndexIsVisible(0);
-			this.setSize(new Dimension(this.getWidth(), this.getHeight()+1)); //hackish...
+			
+			scriptNameLst.getParent().setMinimumSize(new Dimension(maxStringWidth + 10, 1));
+			
+			this.setSize(new Dimension(this.getWidth(), this.getHeight()+flipflop)); //hackish...
+			flipflop *= -1;
 		}
 
 	}
@@ -195,14 +226,18 @@ public class HSP2HF extends JFrame {
 		System.out.println("  Script Arguments: " + (data[4] + (data[5]<<8)));
 		System.out.println("  Script Format Version: " + (data[6] + (data[7]<<8)));
 		System.out.println("  String Table Offset: " + Integer.toHexString(data[8]) + "  " + Integer.toHexString(data[9]) + "  " + Integer.toHexString(data[10]) + "  " + Integer.toHexString(data[11]));*/
+		
+		atIDToScriptSrc.clear();
+		currScriptSrc = null;
+		scriptContentsPnl.removeAll();
 		for (int i=12; i<data.length;) {
 			//Read kind, ID, length, & args; advance pointers.
-			int startsAt = i-12;
-			int kind = data[i++] + (data[i++]<<8) + (data[i++]<<16) + (data[i++]<<24);
-			int id = data[i++] + (data[i++]<<8) + (data[i++]<<16) + (data[i++]<<24);
+			int startsAt = (i-12)/4;
+			int kind = (data[i++]&0xFF) | ((((int)data[i++])*0x100)&0xFF00) | ((((int)data[i++])*0x10000)&0xFF0000) | (((int)(data[i++])*0x1000000)&0xFF000000);
+		    int id = (data[i++]&0xFF) | ((((int)data[i++])*0x100)&0xFF00) | ((((int)data[i++])*0x10000)&0xFF0000) | (((int)(data[i++])*0x1000000)&0xFF000000);
 			int[] args =  null;
 			if (kind==2 || kind==5 || kind==6 || kind==7)
-				args = new int[data[i++] + (data[i++]<<8) + (data[i++]<<16) + (data[i++]<<24)];
+				args = new int[(data[i++]&0xFF) | ((((int)data[i++])*0x100)&0xFF00) | ((((int)data[i++])*0x10000)&0xFF0000) | (((int)(data[i++])*0x1000000)&0xFF000000)];
 			else if (kind==1 || kind==3 || kind==4)
 				args = null;
 			else {
@@ -211,201 +246,233 @@ public class HSP2HF extends JFrame {
 			}
 			if (args!=null) {
 				for (int k=0; k<args.length; k++) {
-					args[k] = (int)data[i++] + (((int)data[i++])<<8) + (((int)data[i++])<<16) + (((int)data[i++])<<24);
+					args[k] = (data[i++]&0xFF) | ((((int)data[i++])*0x100)&0xFF00) | ((((int)data[i++])*0x10000)&0xFF0000) | (((int)(data[i++])*0x1000000)&0xFF000000);
 				}
 			}
 			
-			//Temp: print details
-			System.out.print("@" + startsAt/4 + " ");
+			//Cache the "kind" name.
+			String kindName;
+			switch (kind) {
+				case 1:
+					kindName = "number";
+					break;
+				case 2:
+					kindName = "flow_control";
+					break;
+				case 3:
+					kindName = "global_variable";
+					break;
+				case 4:
+					kindName = "local_variable";
+					break;
+				case 5:
+					kindName = "math";
+					break;
+				case 6:
+					kindName = "built-in";
+					break;
+				case 7:
+					kindName = "script";
+					System.out.println("script: " + id);
+					break;
+				default:
+					kindName = "<unknown>";
+					break;
+			}
+			
+			//Create a "source" snippet
+			StringBuilder srcSnippet = new StringBuilder();
 			if (kind==1) {
-				System.out.print("<" + id + ">      ");
+				srcSnippet.append(id);
 			} else if (kind==3) {
-				System.out.print("var[" + id + "]G   ");
+				srcSnippet.append("var[" + id + "]G");
 			} else if (kind==4) {
-				System.out.print("var[" + id + "]L   ");
+				srcSnippet.append("var[" + id + "]L");
 			} else if (kind==7) {
-				System.out.println(((Script)idToName.get(new Integer(id))).name + "(");
+				srcSnippet.append("<b>"+((Script)idToName.get(new Integer(id))).name + "</b>(");
 				String comma = "";
 				for (int k=0; k<args.length; k++) {
-					System.out.print(comma + "{"+args[k]+"}");
+					srcSnippet.append(comma + "{<a href="+args[k]+">"+args[k]+"</a>}");
 					comma = ",";
 				}
-				System.out.print(");  ");
+				srcSnippet.append(");");
 			} else if (kind==5) {
 				switch (id) {
 					case 0:
-						System.out.print("rand({"+args[0]+"}, {"+args[1]+"})");
+						srcSnippet.append("rand(<a href="+args[0]+">{"+args[0]+"}</a>, <a href="+args[1]+">{"+args[1]+"}</a>);");
 						break;
 					case 1:
-						System.out.print("{"+args[0]+"}**{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}**{"+args[1]+"}");
 						break;
 					case 2:
-						System.out.print("{"+args[0]+"}%{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}%{"+args[1]+"}");
 						break;
 					case 3:
-						System.out.print("{"+args[0]+"}/{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}/{"+args[1]+"}");
 						break;
 					case 4:
-						System.out.print("{"+args[0]+"}*{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}*{"+args[1]+"}");
 						break;
 					case 5:
-						System.out.print("{"+args[0]+"}-{"+args[1]+"}");
+						srcSnippet.append("{<a href="+args[0]+">"+args[0]+"</a>}-{<a href="+args[1]+">"+args[1]+"</a>}");
 						break;
 					case 6:
-						System.out.print("{"+args[0]+"}+{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}+{"+args[1]+"}");
 						break;
 					case 7:
-						System.out.print("{"+args[0]+"}^{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}^{"+args[1]+"}");
 						break;
 					case 8:
-						System.out.print("{"+args[0]+"}|{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}|{"+args[1]+"}");
 						break;
 					case 9:
-						System.out.print("{"+args[0]+"}&{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}&{"+args[1]+"}");
 						break;
 					case 10:
-						System.out.print("{"+args[0]+"}=={"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}=={"+args[1]+"}");
 						break;
 					case 11:
-						System.out.print("{"+args[0]+"}!={"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}!={"+args[1]+"}");
 						break;
 					case 12:
-						System.out.print("{"+args[0]+"}<{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}<{"+args[1]+"}");
 						break;
 					case 13:
-						System.out.print("{"+args[0]+"}>{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}>{"+args[1]+"}");
 						break;
 					case 14:
-						System.out.print("{"+args[0]+"}<={"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}<={"+args[1]+"}");
 						break;
 					case 15:
-						System.out.print("{"+args[0]+"}>={"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}>={"+args[1]+"}");
 						break;
 					case 16:
-						System.out.print("{"+args[0]+"}={"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}={"+args[1]+"}");
 						break;
 					case 17:
-						System.out.print("{"+args[0]+"}+={"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}+={"+args[1]+"}");
 						break;
 					case 18:
-						System.out.print("{"+args[0]+"}+={"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}+={"+args[1]+"}");
 						break;
 					case 19:
-						System.out.print("!{"+args[0]+"}");
+						srcSnippet.append("!{"+args[0]+"}");
 						break;
 					case 20:
-						System.out.print("{"+args[0]+"}&&{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}&&{"+args[1]+"}");
 						break;
 					case 21:
-						System.out.print("{"+args[0]+"}||{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}||{"+args[1]+"}");
 						break;
 					case 22:
-						System.out.print("{"+args[0]+"}^^{"+args[1]+"}");
+						srcSnippet.append("{"+args[0]+"}^^{"+args[1]+"}");
 						break;
 				}
-				System.out.print("   ");
 			} else if (kind==2) {
 				String comma = null;
 				switch (id) {
 					case 0:
-						System.out.print("do(");
+						srcSnippet.append("do(");
 						comma = "";
 						for (int k=0; k<args.length; k++) {
-							System.out.print(comma + "{"+args[k]+"}");
+							srcSnippet.append(comma + "{"+args[k]+"}");
 							comma = ",";
 						}
-						System.out.print(");  ");
+						srcSnippet.append(");");
 						break;
 					case 1:
-						System.out.print("begin (unexpected)");
+						srcSnippet.append("begin (unexpected)");
 						break;
 					case 2:
-						System.out.print("end (unexpected)");
+						srcSnippet.append("end (unexpected)");
 						break;
 					case 3:
-						System.out.println("return(");
+						srcSnippet.append("return(");
 						comma = "";
 						for (int k=0; k<args.length; k++) {
-							System.out.print(comma + "{"+args[k]+"}");
+							srcSnippet.append(comma + "{"+args[k]+"}");
 							comma = ",";
 						}
-						System.out.print(");  ");
+						srcSnippet.append(");  ");
 						break;
 					case 4:
-						System.out.print("if {"+args[0]+"} then {"+args[1]+"} else {"+args[2]+"}  ");
+						srcSnippet.append("if {"+args[0]+"} then {"+args[1]+"} else {"+args[2]+"}");
 						break;
 					case 5:
-						System.out.print("then(");
+						srcSnippet.append("then(");
 						comma = "";
 						for (int k=0; k<args.length; k++) {
-							System.out.print(comma + "{"+args[k]+"}");
+							srcSnippet.append(comma + "{"+args[k]+"}");
 							comma = ",";
 						}
-						System.out.print(");  ");
+						srcSnippet.append(");");
 						break;
 					case 6:
-						System.out.print("else(");
+						srcSnippet.append("else(");
 						comma = "";
 						for (int k=0; k<args.length; k++) {
-							System.out.print(comma + "{"+args[k]+"}");
+							srcSnippet.append(comma + "{"+args[k]+"}");
 							comma = ",";
 						}
-						System.out.print(");  ");
+						srcSnippet.append(");");
 						break;
 					case 7:
-						System.out.print("for(var[{"+args[0]+"}]L i={"+args[1]+"}; i!={"+args[2]+"}; i+={"+args[3]+"}) do {"+args[4]+"};  ");
+						srcSnippet.append("for(var[{"+args[0]+"}]L i={"+args[1]+"}; i!={"+args[2]+"}; i+={"+args[3]+"}) do {"+args[4]+"};");
 						break;
 					case 10:
-						System.out.print("while({"+args[0]+"}) do {"+args[1]+"}  ");
+						srcSnippet.append("while({"+args[0]+"}) do {"+args[1]+"}");
 						break;
 					case 11:
-						System.out.print("break {"+args[0]+"}  ");
+						srcSnippet.append("break {"+args[0]+"}");
 						break;
 					case 12:
-						System.out.print("continue {"+args[0]+"}  ");
+						srcSnippet.append("continue {"+args[0]+"}");
 						break;
 					case 13:
-						System.out.print("exit_script();  ");
+						srcSnippet.append("exit_script();");
 						break;
 					case 14:
-						System.out.print("exit_returning("+args[0]+"});  ");
+						srcSnippet.append("exit_returning("+args[0]+"});");
 						break;
 					case 15:
-						System.out.print("switch(");
+						srcSnippet.append("switch(");
 						comma = "";
 						for (int k=0; k<args.length; k++) {
-							System.out.print(comma + "{"+args[k]+"}");
+							srcSnippet.append(comma + "{"+args[k]+"}");
 							comma = ",";
 						}
-						System.out.print(");  ");
+						srcSnippet.append(");");
 						break;
 					case 16:
-						System.out.print("case (unexpected)");
+						srcSnippet.append("case (unexpected)");
 						break;
 				}
 			} else if (kind==6) {
-				System.out.print("built-in[{"+id+"}](");
+				srcSnippet.append("built-in[{"+id+"}](");
 				String comma = "";
 				for (int k=0; k<args.length; k++) {
-					System.out.print(comma + "{"+args[k]+"}");
+					srcSnippet.append(comma + "{"+args[k]+"}");
 					comma = ",";
 				}
-				System.out.print(");  ");
+				srcSnippet.append(");");
 			} else {
 				//Stack trace
-				System.out.print("????   ");
-				System.out.print(kind + ":" + id + " ");
+				srcSnippet.append(kind + ":" + id + " ");
 				if (args!=null) {
-					System.out.print("[");
+					srcSnippet.append("[");
 					String comma = "";
 					for (int k=0; k<args.length; k++) {
-						System.out.print(comma + args[k]);
+						srcSnippet.append(comma + args[k]);
 						comma = ",";
 					}
-					System.out.print("]");
+					srcSnippet.append("]");
 				}
 			}
-			System.out.println();
+			
+			//Add it to the hashtable, and to the list  
+			ScriptSrc curr = new ScriptSrc(startsAt, atIDToScriptSrc.size(), kindName, srcSnippet.toString());
+			atIDToScriptSrc.put(new Integer(curr.atID), curr);
+			scriptContentsPnl.add(curr);
 		}
 	}
 	
@@ -440,11 +507,16 @@ public class HSP2HF extends JFrame {
 				Script s = (Script)scriptNameLst.getSelectedValue();
 				System.out.println("\n" + s.name);
 				loadScriptSource(s.scriptSrc.data);
+				
+				//Reset size...
+				setSize(new Dimension(getWidth(), getHeight()+flipflop)); //hackish...
+				flipflop *= -1;
 			}
 		});
 		
-		//Right paenl
+		//Right panel
 		scriptContentsPnl = new JPanel();
+		scriptContentsPnl.setLayout(new GridLayout(0, 1));
 		scriptContentsPnl.setBorder(BorderFactory.createTitledBorder("Source"));
 	}
 	
@@ -482,7 +554,7 @@ public class HSP2HF extends JFrame {
 			gbc.anchor = GridBagConstraints.FIRST_LINE_START;
 			gbc.weightx = 0.0;
 			gbc.weighty = 0.0;
-			JScrollPane jsp = new JScrollPane(scriptNameLst);
+			JScrollPane jsp = new JScrollPane(scriptNameLst, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 			jsp.setBorder(null);
 			cp.add(jsp, gbc);
 		}
@@ -499,9 +571,13 @@ public class HSP2HF extends JFrame {
 			gbc.ipady = 2;
 			gbc.insets = new Insets(2, 2, 2, 2);
 			gbc.anchor = GridBagConstraints.FIRST_LINE_START;
-			gbc.weightx = 0.75;
+			gbc.weightx = 0.5;
 			gbc.weighty = 1.0;
-			cp.add(scriptContentsPnl, gbc);
+			JPanel jp = new JPanel(new BorderLayout());
+			jp.add(scriptContentsPnl, BorderLayout.NORTH);
+			//jp.add(new JPanel(), BorderLayout.CENTER);
+			JScrollPane jsp = new JScrollPane(jp, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+			cp.add(jsp, gbc);
 		}
 	}
 	
@@ -570,6 +646,113 @@ public class HSP2HF extends JFrame {
 			return "[" + id + "]" + " " + name + "(" + args.length + " args)";
 		}
 	}
+	
+	
+	class ScriptSrc extends JPanel {
+		private int atID;
+		private String kindName;
+		private String srcSnippet;
+		private int realID;
+		private String bgColor;
+		
+		private JEditorPane scriptSrcLbl;
+		private JPanel upperPnlLeft;
+		
+		public ScriptSrc(int atID, int realID, String kindName, String srcSnippet) {
+			this.atID = atID;
+			this.realID = realID;
+			this.kindName = kindName;
+			this.srcSnippet = srcSnippet;
+			initPnl();
+		}
+		
+		public void flashID(boolean on) {
+			Color c = Color.yellow;
+			if (!on)
+				c = this.getBackground();
+			
+			upperPnlLeft.setBackground(c);
+		}
+		
+		private void initPnl() {
+			int bgColor = 0xD0FFE6;
+			if (this.realID%2==0)
+				bgColor = 0xBBEEFF;
+			this.setBackground(new Color(bgColor));
+			this.bgColor = ""+Integer.toHexString(bgColor).toUpperCase();
+			//System.out.println(this.bgColor);
+			this.setPreferredSize(new Dimension(300, 50));
+			this.setLayout(new BorderLayout());
+			this.setBorder(BorderFactory.createLineBorder(Color.darkGray, 1));
+			this.addMouseListener(new MouseAdapter() {
+				public void mousePressed(MouseEvent e) {
+					swapIn(ScriptSrc.this);
+				}
+			});
+			
+			scriptSrcLbl = new JEditorPane("text/html", "<html><head><style type='text/css'>body {padding: 3 3 0 3; background-color: #"+this.bgColor+";}  a {text-decoration: underline; font-weight: bold; color: #FF2222;}</style></head><body><span style='font-family: Courier; font-weight: normal; font-size: 12pt;'>" + this.srcSnippet + "</span></body></html>");
+			scriptSrcLbl.setMargin(new Insets(0, 0, 0, 0));
+			scriptSrcLbl.setEditable(false);
+			scriptSrcLbl.addHyperlinkListener(new HyperlinkListener() {
+				public void hyperlinkUpdate(HyperlinkEvent ev) {
+					if (ev.getEventType()==HyperlinkEvent.EventType.ACTIVATED)  {
+						try {
+							int x = Integer.parseInt(ev.getDescription());
+							swapIn(((ScriptSrc)atIDToScriptSrc.get(new Integer(x))));
+						} catch (NumberFormatException ex) {}
+					}
+				}
+			});
+			
+			JPanel lowerPnl = new JPanel(new BorderLayout());
+			lowerPnl.setBackground(this.getBackground());
+			//lowerPnl.setBorder(BorderFactory.createLineBorder(Color.lightGray, 2));
+			JLabel srcLbl = new JLabel("Source");
+			srcLbl.setFont(new Font("Arial", Font.ITALIC, 12));
+			//lowerPnl.add(srcLbl, BorderLayout.NORTH);
+			//lowerPnl.add(scriptSrcLbl, BorderLayout.NORTH);
+			this.add(scriptSrcLbl, BorderLayout.CENTER);
+			
+			upperPnlLeft = new JPanel(new BorderLayout());
+			upperPnlLeft.setBackground(this.getBackground());
+			upperPnlLeft.setBorder(BorderFactory.createLineBorder(Color.lightGray, 2));
+			JLabel atLbl = new JLabel(" @");
+			atLbl.setFont(new Font("Arial", Font.BOLD, 8));
+			JLabel atValLbl = new JLabel(atID+" ");
+			atValLbl.setFont(new Font("Arial", Font.PLAIN, 16));
+			upperPnlLeft.add(atLbl, BorderLayout.WEST);
+			upperPnlLeft.add(atValLbl, BorderLayout.CENTER);
+			
+			JPanel upperPnlRight = new JPanel(new BorderLayout());
+			upperPnlRight.setBackground(this.getBackground());
+			JLabel typeLbl = new JLabel(" ");
+			typeLbl.setFont(new Font("Arial", Font.ITALIC, 12));
+			JLabel typeValLbl = new JLabel(kindName);
+			typeValLbl.setFont(new Font("Arial", Font.ITALIC, 12));
+			upperPnlRight.add(typeLbl, BorderLayout.WEST);
+			upperPnlRight.add(typeValLbl, BorderLayout.CENTER);
+			
+			JPanel upperPanl = new JPanel(new BorderLayout());
+			upperPanl.setBackground(this.getBackground());
+			upperPanl.add(upperPnlLeft, BorderLayout.WEST);
+			upperPanl.add(upperPnlRight, BorderLayout.CENTER);
+			this.add(upperPanl, BorderLayout.NORTH);
+		}
+		
+		public int getAtID() {
+			return this.atID;
+		}
+		public int getRealID() {
+			return this.realID;
+		}
+		public String getKindName() {
+			return this.kindName;
+		}
+		public String getSrcSnippet() {
+			return this.srcSnippet;
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		new HSP2HF();
